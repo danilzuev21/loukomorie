7d45de379a81d8765a901ef03fb35d120541923e
unknown <a9500484054@yandex.ru>
2023-03-05 20:24:46 +0300

    dsasd

--- js/app.js
+++ js/app.js
@@ -14,6 +14,15 @@ const bodyElement = document.body
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    document.querySelector('.menu-burger').addEventListener('click', ()=> {
+        document.querySelector('.menu').classList.add('menu--active');
+    });
+
+    document.querySelector('.menu__btn-close').addEventListener('click', ()=> {
+        document.querySelector('.menu').classList.remove('menu--active');
+    });
+
+
     const swiper1 = new Swiper('.advantages__slider', {
         pagination: {
             el: ".swiper-pagination",
@@ -27,6 +36,59 @@ document.addEventListener('DOMContentLoaded', () => {
           },
     });
     const swiper3 = new Swiper('.foto__slider', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            // when window width is >= 640px
+            1240: {
+                slidesPerView: 3,
+                spaceBetween: 40
+            }
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    const swiper4 = new Swiper('.tither__slider', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            // when window width is >= 480px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    const swiper5 = new Swiper('.roboxod__slider', {
         slidesPerView: 1,
         spaceBetween: 20,
         breakpoints: {
